6e6a06db9d3e3090a627eedad439c76d2ea5af2b
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-20 09:17:28 +0100

    Created ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,21 @@
+function myRecursion(arr, n){
+
+    var arr;
+    var n;
+    
+    if(n<=0){
+        return 0;
+
+    }
+    else {
+        return  myRecursion(arr, n-1)+ arr[n-1];
+    }
+
+    
+}
+
+console.log(myRecursion([1],0));
+console.log(myRecursion([1, 2, 3, 4],2));
+console.log(myRecursion([1, 2, 3, 4],3));
+console.log(myRecursion([1, 2, 3, 4, 5, 6],5));
+module.exports=myRecursion;
