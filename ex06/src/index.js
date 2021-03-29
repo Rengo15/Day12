e5868c9bb8d61ee9ea7eb111021882b0e1d4beb2
Rengo15 <adnan.hamzagic11@gmail.com>
2021-03-29 14:42:49 +0200

    Created ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,21 @@
+function myNes() {
+    var myStorage = {
+        "car" : {
+            "inside": {
+                "glove box": "maps",
+                "passanger seat" : "crumbs"
+            },
+            "outside" : {
+                "trunk": "jack"
+            }
+        }
+    };
+    var gloveBoxContents = myStorage.car.inside["glove box"];
+    return gloveBoxContents;
+    // Only change code below this line 
+
+    // Only change code above this line
+}
+
+console.log(myNes());
+module.exports = myNes;
